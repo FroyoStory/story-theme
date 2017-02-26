704ba539e4f27fe255dbab0edf40e7d8902e391a
Purwandi <free6300@gmail.com>
2017-02-26 17:57:32 +0700

    Update backend template

--- views/layouts/master.blade.php
+++ views/layouts/master.blade.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>@yield('title')</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="csrf-token" content="{{ session()->token() }}"/>
-  @yield('meta')
-</head>
-<body>
-  <div class="menu-wrapper"></div>
-  <div class="page-wrapper">
-    @yield('content')
-  </div>
-  <div class="admin__menu-overlay"></div>
-  <div class="modal-wrapper"></div>
-</body>
+  <head>
+    <title>@yield('title')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ session()->token() }}"/>
+    <link rel="stylesheet" href="/css/backend.css">
+    @yield('meta')
+  </head>
+  <body>
+    <div class="menu-wrapper fixed">
+      @include('story-theme::layouts.partials.nav-sidebar')
+    </div>
+    <div class="page-wrapper">
+      @yield('content')
+    </div>
+    <div class="admin__menu-overlay"></div>
+    <div class="modal-wrapper"></div>
+
+    <script src="/js/app.js"></script>
+  </body>
 </html>
--- views/layouts/partials/nav-sidebar.blade.php
+++ views/layouts/partials/nav-sidebar.blade.php
@@ -1,20 +1,35 @@
-<nav>
-  <ul id="nav" role="menubar">
-    <li>
-      <a href=""></a>
-      <div class="submenu">
-        <strong>Sales</strong>
-        <ul role="menu">
-          <li>
-            <strong><span>Operations</span></strong>
-            <div class="submenu">
-              <ul class="role">
-                <li></li>
-              </ul>
+<nav class="">
+  <ul>
+    <li><a href="" class="main-menu"><i class="material-icons">album</i> <span>STORY</span></a></li>
+  </ul>
+  <ul id="nav" role="menubar" class="sidebar-menu">
+    @foreach (Navigation::get('backend') as $key => $navigation)
+    <li data-submenu-id="submenu-{{ $key }}">
+      <a href="#" class="main-menu">
+        <i class="material-icons">{{ $navigation['font'] }}</i>
+        <span>{{ $navigation['title'] }}</span>
+      </a>
+
+      @if (isset($navigation['groups']))
+        <div class="popover" id="submenu-{{ $key }}">
+          <h3 class="popover-title">{{ $navigation['title'] }}</h3>
+          <div class="popover-content">
+            <div class="popover-content-column">
+              @foreach ($navigation['groups'] as $key_group => $group)
+                <div class="popover-content-grid">
+                  <h5>{{ Str::title($key_group) }}</h5>
+                  <ul>
+                    @foreach ($group as $item)
+                    <li><a href="/backend/{{ $key }}/{{$key_group}}/{{$item}}">{{ Str::title($item) }}</a></li>
+                    @endforeach
+                  </ul>
+                </div>
+              @endforeach
             </div>
-          </li>
-        </ul>
-      </div>
+          </div>
+        </div>
+      @endif
     </li>
+    @endforeach
   </ul>
 </nav>

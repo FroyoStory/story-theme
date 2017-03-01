ff8cfc087d27acc7eb75aa0ea8d65e030900a532
Purwandi <free6300@gmail.com>
2017-03-01 16:17:15 +0700

    Add editor layouts

---
+++ views/layouts/editor.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ session()->token() }}"/>
+    <link rel="stylesheet" href="/css/backend.css">
+    @yield('meta')
+  </head>
+  <body>
+    <div class="menu-wrapper fixed">
+      @include('story-theme::layouts.partials.nav-sidebar')
+    </div>
+    <div class="page-wrapper">
+      @yield('content')
+    </div>
+    <div class="admin__menu-overlay"></div>
+    <div class="modal-wrapper"></div>
+
+    <script src="/js/app.js"></script>
+    <script src="/vendor/ckeditor/ckeditor.js"></script>
+    <script>
+     $(document).ready(function() {
+      CKEDITOR.replace('body')
+     })
+    </script>
+  </body>
+</html>

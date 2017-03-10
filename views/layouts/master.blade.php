d731cc2a428686f30006b7ab0ed7f3aed10890cc
Purwandi <free6300@gmail.com>
2017-03-10 11:56:17 +0700

    Update templates

--- views/layouts/master.blade.php
+++ views/layouts/master.blade.php
@@ -12,12 +12,33 @@
       @include('story-theme::layouts.partials.nav-sidebar')
     </div>
     <div class="page-wrapper">
+      <!-- HEADER AREA -->
+      <div class="page-header">
+        <div class="page-header-content">
+          <div class="page-title"><h1>@yield('title')</h1></div>
+          @yield('heading-elements')
+        </div>
+      </div>
+      <!-- END HEADER AREA -->
+
       @include('story-theme::layouts.partials.flash-message')
       @yield('content')
     </div>
+
     <div class="admin__menu-overlay"></div>
     <div class="modal-wrapper"></div>
 
     <script src="/js/backend.js"></script>
+    <script src="/vendor/ckeditor/ckeditor.js"></script>
+    <script>
+     $(document).ready(function() {
+      CKEDITOR.replace('body', {
+        filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+        filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token={{csrf_token()}}',
+        filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+        filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token={{csrf_token()}}'
+      })
+     })
+    </script>
   </body>
 </html>
